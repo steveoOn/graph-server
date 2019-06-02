a62f31dd9c1faab07a88312e52afdef34f0f20d4
Steven Ren <rsw_mmo@hotmail.com>
2019-06-02 20:31:05 +0800

    graphql example

---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <div>
+      <ul id="book-list">
+        <li>book name</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
